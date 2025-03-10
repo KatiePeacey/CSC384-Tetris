7b8a8e13de2c412fd32bbde528f9c6daffa7dbe7
KatiePeacey <katiepeacey@outlook.com>
2025-03-10 18:52:29 +0000

    Adding line clears and game over reset

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -82,6 +82,7 @@ public class Piece : MonoBehaviour
     private void Lock()
     {
         this.board.Set(this);
+        this.board.ClearLines();
         this.board.SpawnPiece();
     }
     private bool Move(Vector2Int translation)
